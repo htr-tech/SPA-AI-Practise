347da9beb07e11620f553f3f766b4d153dcb284e
htr-tech <tahmidrayat@gmail.com>
2025-09-26 10:17:44 +0600

    refactor: moved router config to seperate file

---
+++ js/main.routes.js
@@ -0,0 +1,15 @@
+const STATIC_SECTIONS = ['home', 'private', 'new'];
+const DYNAMIC_ROUTES = {
+    'home-flag': {
+        file: 'html/flagquiz.html',
+        parent: 'home',
+        title: 'Country Flag Quiz'
+    },
+
+    'new-idiom': {
+        file: 'html/idiomdict.html',
+        parent: 'new',
+        title: 'Idioms Library'
+    },
+
+};
